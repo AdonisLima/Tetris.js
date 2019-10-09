6ff86f65e116cc528cfb310ba584668cd580c261
unknown <adonis.lima@LAB-DEINFO-09.deinfo.lab33>
2019-10-09 16:08:31 -0300

    Now all tetrominos can be drawn, but in black color
    Signed-off-by: unknown <adonis.lima@LAB-DEINFO-09.deinfo.lab33>

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const heightInBlocks = 20;
 const blockWidth = widthInPixels / widthInBlocks;
 const blockHeight = heightInPixels / heightInBlocks;
 
+//TODO change all functions to arrow functions
+
 class GameField {
     constructor(grid) {
         this._grid = grid;
@@ -32,12 +34,15 @@ class GameField {
     }
 }
 
+//The x represents the leftmost block of the piece to be rendered
+//The y represents the uppermost block of the piece to be rendered
 class Piece {
-    constructor(id, x, y, shape) {
+    constructor(id, x, y, shape, color) {
         this._id = id;
         this._x = x;
         this._y = y;
         this._shape = shape;
+        this._color = color;
     }
 
     get id() {
@@ -64,10 +69,19 @@ class Piece {
         return this._shape;
     }
 
-    set shape(shape) {
-        this._shape = shape;
+    set shape(newShape) {
+        this._shape = newShape;
     }
 
+    get color() {
+        return this._color;
+    }
+
+    set color(newColor) {
+        this._color = newColor;
+    }
+
+    //Apply offset to the rotations
     rotate90deg() {
         //TODO
     }
@@ -95,6 +109,9 @@ function initMatrix() {
     return field;
 }
 
+
+//Draws a single block given the coordinates x and y
+//TODO use rest operator
 function drawBlock(x, y) {
     context.fillRect(blockWidth * x, blockHeight * y, blockWidth - 1, blockHeight - 1);
     context.strokeRect(blockWidth * x, blockHeight * y, blockWidth - 1, blockHeight - 1);
@@ -120,43 +137,86 @@ function init() {
 }
 
 init();
-// field.grid = GameField.initMatrix(widthInBlocks, heightInBlocks);
-//console.log(matrix);
 
 let pI = new Piece(0, 3, 0,
-    [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-    ]
+    [1, 1, 1, 1],
+    '#27E2F4'//Light blue
 );
 
-//TODO instantiate the other pieces
+let pJ = new Piece(1, 3, 0,
+    [2, 0, 0, 2, 2, 2],
+    '#1701FE' //Dark Blue
+);
 
-//TODO function thet randomize pieces
+let pL = new Piece(2, 3, 0,
+    [0, 0, 3, 3, 3, 3],
+    '#E46F02' //Orange
+);
+
+let pO = new Piece(3, 3, 0,
+    [0, 4, 4, 0, 4, 4],
+    '#F4D800' //Yellow
+);
+
+let pS = new Piece(4, 3, 0,
+    [0, 5, 5, 5, 5, 0],
+    '#7BF800' //Green
+);
+
+let pZ = new Piece(5, 3, 0,
+    [6, 6, 0, 0, 6, 6],
+    '#D40D3A' //Red
+);
+
+let pT = new Piece(6, 3, 0,
+    [0, 5, 0, 5, 5, 5],
+    '#9B00FC' //Purple
+);
+
+//TODO function that randomize pieces
+/*
+    Random Generator
+The Random Generator (also known as "random bag" or "7 bag") determines the sequence
+ of tetrominoes during gameplay. One of each of the 7 tetrominoes are shuffled in a 
+ "bag", and are dealt out one by one. When the bag is empty, a new one is filled 
+ and shuffled.
+    source: https://tetris.wiki/Tetris_Guideline
+*/
 
 //TODO draw the first piece and put it in the array
-
 function spawnTetro(tetro) {
     current = tetro;
-    let rows = current.shape.length;
-    let columns = current.shape[0].length;
+    let dataLength = current.shape.length;
     let posX = current.x;
     let posY = current.y;
-    console.log(rows, columns);
-    
-    // x ~ j
-    //y ~ i
-    for(let i = 0; i < rows; i++){
-        for(let j = 0; j < columns; j++){
-            if(current.shape[i][j]){
-                //TODO DRAW BLOCKS 
-                drawBlock(posX+j,posY);
-            } 
+    console.log(dataLength);
+
+    // x ~ j ~ row
+    //y ~ i ~ col
+    //TODO draw any piece with that 
+
+    let j = 0;
+    let x = 0;
+    for (let i = 0; i < dataLength; i++) {
+        console.log(x, j);
+        if (current.shape[i]) {
+            drawBlock(posX + x, posY + j);
         }
+
+        if ((i + 1) % (dataLength % 3 == 0 ? 3 : 4) === 0) {
+            j++;
+            x = 0;
+        } else {
+            x++;
+        }
+
     }
-    
+
+
+    //Get the coordinates and print using the piece spawn coordinate
+    // console.log(blocksCoordinates);
 }
 
-spawnTetro(pI);
+//spawnTetro(pS);
+//spawnTetro(pZ);
+//spawnTetro(pT);
