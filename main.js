71acc2ae39433e295f9bd0a27bbd550fdff169c7
unknown <ADONIS.LIMA@LAB-DEINFO-17.deinfo.lab33>
2019-10-29 11:54:40 -0300

    Tetrominos can collide with each other.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let canvas = [];
 let context = [];
 let field = [];
-let current = {};
+let current = [];
 let score = 0;
+let lose = 0;
 
 const tick = 300;
 const widthInPixels = 300;
@@ -23,7 +24,7 @@ let initCanvas = () => {
 
 //Cria field vazia do campo do jogo.
 let createField = () => {
-    for(let i = 0; i < heightInBlocks; i++){
+    for (let i = 0; i < heightInBlocks; i++) {
         field[i] = new Array(widthInBlocks).fill(0);
     }
 };
@@ -51,60 +52,60 @@ let pI = new Piece(1, 3, 0,
 
 let pJ = new Piece(2, 3, 0,
     [
-        [0,0],
-        [1,0],
-        [1,1],
-        [1,2]
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [1, 2]
     ],
     '#1701FE' //Dark Blue
 );
 
 let pL = new Piece(3, 3, 0,
     [
-        [0,2],
-        [1,0],
-        [1,1],
-        [1,2],
+        [0, 2],
+        [1, 0],
+        [1, 1],
+        [1, 2],
     ],
     '#E46F02' //Orange
 );
 
 let pO = new Piece(4, 3, 0,
     [
-        [0,0],
-        [0,1],
-        [1,0],
-        [1,1],
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
     ],
     '#F4D800' //Yellow
 );
 
 let pS = new Piece(5, 3, 0,
     [
-        [0,1],
-        [0,2],
-        [1,0],
-        [1,1]
+        [0, 1],
+        [0, 2],
+        [1, 0],
+        [1, 1]
     ],
     '#7BF800' //Green
 );
 
 let pZ = new Piece(6, 3, 0,
     [
-        [0,0],
-        [0,1],
-        [1,1],
-        [1,2]
+        [0, 0],
+        [0, 1],
+        [1, 1],
+        [1, 2]
     ],
     '#D40D3A' //Red
 );
 
 let pT = new Piece(7, 3, 0,
     [
-        [0,1],
-        [1,0],
-        [1,1],
-        [1,2]
+        [0, 1],
+        [1, 0],
+        [1, 1],
+        [1, 2]
     ],
     '#9B00FC' //Purple
 );
@@ -119,94 +120,21 @@ The Random Generator (also known as "random bag" or "7 bag") determines the sequ
     source: https://tetris.wiki/Tetris_Guideline
 */
 
-// let drawTetro = () => {
-
-//     let col = current.colPos,
-//         row = current.rowPos,
-//         tetroLength = current.shape.length,
-//         previousTetroColPosition = 0,
-//         previousTetroRowPosition = 0,
-//         isFirstBlockDrawn = false;
-
-//     for (let i = 0; i < tetroLength; i++) {
-//         for (let j = 0; j < tetroLength; j++) {
-//             if (current.shape[i][j]) {
-//                 if (!isFirstBlockDrawn) {
-//                     drawBlock(row, col);
-//                     isFirstBlockDrawn = !isFirstBlockDrawn;
-//                 } else {
-//                     col += (j - previousTetroColPosition);
-//                     row += (i - previousTetroRowPosition);
-//                     drawBlock(row, col);
-//                 }
-
-//                 previousTetroColPosition = j;
-//                 previousTetroRowPosition = i;
-//             }
-//         }
-//     }
-// };
-
 let drawTetro = () => {
-    for(let i = 0; i < current.coordinates.length; i++){
-        let row = current.coordinates[i][0] + current.rowPos;
-        let col = current.coordinates[i][1] + current.colPos;
-        console.log("Row " + row, "Col " + col);
-        drawBlock(row,col);
-        field[row][col] = current.id;
+    for (let i = 0; i < current.coordinates.length; i++) {
+        let row = current.coordinates[i][0] + current.startRowPos;
+        let col = current.coordinates[i][1] + current.startColPos;
+        drawBlock(row, col);
+        // field[row][col] = 1/* current.id */;
     }
-    console.log("---");
 };
-
-// let getCoordinates = () => {
-//     let col = current.colPos,
-//         row = current.rowPos,
-//         tetroLength = current.shape.length,
-//         previousTetroColPosition = 0,
-//         previousTetroRowPosition = 0,
-//         isFirstBlockIdentified = false;
-
-//     currentCoordinates = [];
-
-//     for (let i = 0; i < tetroLength; i++) {
-//         for (let j = 0; j < tetroLength; j++) {
-//             if (current.shape[i][j]) {
-//                 if (!isFirstBlockIdentified) {
-//                     isFirstBlockIdentified = !isFirstBlockIdentified;
-//                 } else {
-//                     col += (j - previousTetroColPosition);
-//                     row += (i - previousTetroRowPosition);
-//                 }
-
-//                 currentCoordinates.push([row, col]);
-
-//                 previousTetroColPosition = j;
-//                 previousTetroRowPosition = i;
-//             }
-//         }
-//     }
-
-//     return currentCoordinates;
-// };
-
-// let printGrid = field => {
-//     let rows = field.length;
-//     let cols = field[0].length;
-
-//     for (let i = 0; i < rows; i++) {
-//         for (let j = 0; j < cols; j++) {
-//             document.write(`${field[i][j]}`);
-//         }
-//         document.write(`</br>`);
-//     }
-// };
 
 function newPiece() {
     current = new Piece();
-    current = Object.assign(current, arr[Math.floor(Math.random() * arr.length)]);
+    current = Object.assign(current,
+        arr[Math.floor(Math.random() * arr.length)]);
 }
 
-//Warning: essa funcção só é destinada a Objetos Piece
 function isEmpty(obj = current) {
     for (let _id in obj) {
         if (obj.hasOwnProperty(_id)) {
@@ -216,119 +144,150 @@ function isEmpty(obj = current) {
     return true;
 }
 
-//Check if position is valid yet
-//Cases:
-// let isValid = () => {
-
-// };
-
-//Check if there's no more room for the piece to move
-//Edge cases: Borders
-let isBlocked = () => {
-    for (i = 0; i < current.coordinates.length; i++) {
-        for (j = 0; j < current.coordinates[i].length; j++) {
-            if (current.coordinates[i][0] == heightInBlocks - 1) {
-                return true;
-            }
-        }
-    }
-
-    //TODO: TRATAMENTO PARA AS OUTRAS BORDAS
-
-    //TODO: TRATAMENTO PARA PEÇA QUE COLIDE COM AS ANTIGAS,
-    //1: get coordinates from the last pieces of its respective columns
-    //How: get the biggest difference of each column
-    return false;
-};
-
 let clearBlock = (row, col) => {
-    context.clearRect(blockWidth * col, blockHeight * row, blockWidth - 1, blockHeight - 1);  
+    context.clearRect(blockWidth * col, blockHeight * row, blockWidth - 1, blockHeight - 1);
 };
 
 let clearTetro = () => {
-    // current.coordinates.forEach(e => {
-    //     let row = e[0] + current.rowPos;
-    //     let col = e[1] + current.colPos;
-    //     clearBlock(row, col);
-    //     field[row][col] = 0;
-    // });
-
-    for(let i = 0; i < current.coordinates.length; i++){
-        let row = current.coordinates[i][0] + current.rowPos;
-        let col = current.coordinates[i][1] + current.colPos;
+    for (let i = 0; i < current.coordinates.length; i++) {
+        let row = current.coordinates[i][0] + current.startRowPos;
+        let col = current.coordinates[i][1] + current.startColPos;
         clearBlock(row, col);
         field[row][col] = 0;
     }
-    
+
 };
 
 let clearAll = () => {
     context.clearRect(1, 1, canvas.width - 2, canvas.height - 2);
 };
 
-
 let update = () => {
     if (isEmpty()) {
         newPiece();
-
+        drawTetro();
     } else {
-        clearTetro();
+        moveDown();
     }
-
-    drawTetro();
-    clearInterval(game);
-    // clearInterval(game);
-    //TODO INSERT CODE THAT CHECK IF THERE'S MORE ROOM TO SLIDE
-    //Check space goes down here
-    if (isBlocked(current.coordinates)) {
-        // settleDown(current.coordinates);
-        // current = {};
-        // clearTetro(current.coordinates);
-        clearInterval(game);
-    }
-
 };
 
 init();
 
 document.addEventListener('keydown', event => {
-    if(event.keyCode == 37){
-        moveLeft();
-    }
-    if (event.keyCode == 39) {
-        moveRight();
-    }
-    if (event.keyCode == 40){
-        moveDown();
+    if (!lose) {
+        if (event.keyCode == 38) {
+            rotate();
+        } else if (event.keyCode == 37) {
+            moveLeft();
+        } else if (event.keyCode == 39) {
+            moveRight();
+        } else if (event.keyCode == 40) {
+            moveDown();
+        }
     }
 });
 
 let moveLeft = () => {
-    // clearCurrentTetro();
-    clearTetro();
-    current.colPos--;
-    drawTetro();
+    let hasRoom = true;
+    for (let coordinate of current.coordinates) {
+        let col = coordinate[1] + current.startColPos;
+        if (col == 0) {
+            hasRoom = false;
+            break;
+        }
+    }
+
+    for (let i = 0; i < current.coordinates.length; i++) {
+        let row = current.coordinates[i][0] + current.startRowPos;
+        let col = current.coordinates[i][1] + current.startColPos;
+        if (field[row][col - 1] == 1) {
+            hasRoom = false;
+        }
+    }
+
+    if (hasRoom) {
+        clearTetro();
+        current.startColPos--;
+        drawTetro();
+    }
+
 };
 
 let moveRight = () => {
-    // clearCurrentTetro();
-    clearTetro();
-    current.colPos++;
-    drawTetro();
+    let hasRoom = true;
+    for (let coordinate of current.coordinates) {
+        let col = coordinate[1] + current.startColPos;
+        if (col == widthInBlocks - 1) {
+            hasRoom = false;
+            break;
+        }
+    }
+
+    //Check for collision to the right
+    for (let i = 0; i < current.coordinates.length; i++) {
+        let row = current.coordinates[i][0] + current.startRowPos;
+        let col = current.coordinates[i][1] + current.startColPos;
+        if (field[row][col + 1] == 1) {
+            hasRoom = false;
+        }
+    }
+
+
+    //Check for collision with another tetros
+
+    if (hasRoom) {
+        clearTetro();
+        current.startColPos++;
+        drawTetro();
+    }
 };
 
 let moveDown = () => {
-    // clearCurrentTetro();
-    clearTetro();
-    current.rowPos++;
-    drawTetro();
+    let hasReachedBottom = false;
+    for (let coordinate of current.coordinates) {
+        let row = coordinate[0] + current.startRowPos;
+        if (row == heightInBlocks - 1) {
+            hasReachedBottom = true;
+
+        }
+    }
+
+    //Check for collision below
+    let isCollidedBelow = false;
+    if (!hasReachedBottom) {
+        for (let i = 0; i < current.coordinates.length; i++) {
+            let row = current.coordinates[i][0] + current.startRowPos;
+            let col = current.coordinates[i][1] + current.startColPos;
+            if (field[row + 1][col] == 1) {
+                isCollidedBelow = true;
+            }
+        }
+    }
+
+    if (!hasReachedBottom && !isCollidedBelow) {
+        clearTetro();
+        current.startRowPos++;
+        drawTetro();
+    }
+    if (isCollidedBelow || hasReachedBottom) {
+        settleDown();
+        current = [];
+    }
 };
 
-const movement = {
-    "default": 1,
-    "right": 2,
-    "left": 3,
+let settleDown = () => {
+    for (let i = 0; i < current.coordinates.length; i++) {
+        let row = current.coordinates[i][0] + current.startRowPos;
+        let col = current.coordinates[i][1] + current.startColPos;
+        field[row][col] = 1;
+    }
 };
+
+// const movement = {
+//     "default": 1,
+//     "right": 2,
+//     "left": 3,
+// };
 
 //Teste com todas as peças
 let arr = new Array();
@@ -352,4 +311,3 @@ let game = setInterval(update, tick);
 //     });
 // }
 // printMatrixBtn();
-
--- piece.js
+++ piece.js
@@ -1,8 +1,8 @@
 class Piece {
-    constructor(id, colPos, rowPos, coordinates, color) {
+    constructor(id, startColPos, startRowPos, coordinates, color) {
         this._id = id;
-        this._colPos = colPos;
-        this._rowPos = rowPos;
+        this._startColPos = startColPos;
+        this._startRowPos = startRowPos;
         this._coordinates = coordinates;
         this._color = color;
     }
@@ -11,20 +11,20 @@ class Piece {
         return this._id;
     }
 
-    get colPos() {
-        return this._colPos;
+    get startColPos() {
+        return this._startColPos;
     }
 
-    set colPos(newColPos) {
-        this._colPos = newColPos;
+    set startColPos(newStartColPos) {
+        this._startColPos = newStartColPos;
     }
 
-    get rowPos() {
-        return this._rowPos;
+    get startRowPos() {
+        return this._startRowPos;
     }
 
-    set rowPos(newRowPos) {
-        this._rowPos = newRowPos;
+    set startRowPos(newStartRowPos) {
+        this._startRowPos = newStartRowPos;
     }
 
     get coordinates() {
@@ -42,44 +42,5 @@ class Piece {
     set color(newColor) {
         this._color = newColor;
     }
-
-    rotate() {
-        let m = this.coordinates;
-        console.log(m);
-        let n = m.length;
-        
-
-        let a = (m) =>{
-            for(let i = 0; i < m.length; i++){
-                for(let j = 0; j < m.length; j++){
-                    if(m[i][j]){
-                        m.rowPos = i;
-                        m.colPos = j;
-                        return;
-                    }
-                }
-            }
-        };
-        a(m);   
-        
-
-        for (let i = 0; i < Math.floor(n / 2); i++) {
-            for (let j = 0; j < n - (2 * i) - 1; j++) {
-                let t = m[i + j][n - 1 - i]; // t = B
-                m[i + j][n - 1 - i] = m[i][i + j]; //B = A
-                m[i][i + j] = t; //A = t
-
-                t = m[n - 1 - i][n - 1 - i - j];
-                m[n - 1 - i][n - 1 - i - j] = m[i][i + j];
-                m[i][i + j] = t;
-
-                t = m[n - 1 - i - j][i];
-                m[n - 1 - i - j][i] = m[i][i + j];
-                m[i][i + j] = t;
-            }
-        }
-
-        
-    }
-
+    
 }
